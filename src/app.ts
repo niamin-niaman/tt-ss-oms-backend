afff2b1c36027cf145f92a50160c93aa0ec65465
niamin-niaman <niamin.niaman@gmail.com>
2024-10-07 01:15:07 +0700

    add que

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
+import nodemailer from 'nodemailer';
+import Bull, { Job } from 'bull';
 
 
 const app = express();
@@ -8,6 +10,20 @@ app.use(bodyParser.json())
 
 const prisma = new PrismaClient()
 
+const checkQueue = new Bull("check", {
+    redis: "default:Qrv0pEDSNKKzmWKnuCOS872zBHBtT7Jd@redis-11889.c252.ap-southeast-1-1.ec2.redns.redis-cloud.com:11889"
+});
+
+const check = async (randomId: any) => {
+    checkQueue.add({ randomId })
+}
+const processCheck = async (job: Job) => {
+    const { randomId } = job.data
+    setTimeout(() => {
+        console.log(`running ${randomId}`)
+    }, 1000 * randomId)
+}
+checkQueue.process(processCheck);
 
 app.get("/", async (req, res) => {
 
@@ -19,9 +35,51 @@ app.get("/", async (req, res) => {
 
     res.json({
         "message": "OK !!",
-        "db":testDB 
+        "db": testDB
     })
-    
+
+})
+
+app.post("/check_email", async (req, res) => {
+
+    const { to } = req.body
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.resend.com',
+        secure: true,
+        port: 465,
+        auth: {
+            user: 'resend',
+            pass: process.env.RESEND_API_KEY,
+        },
+    });
+
+    const info = await transporter.sendMail({
+        from: 'oms.backend@niamin.dev',
+        to: to,
+        subject: 'Hello World',
+        html: '<strong>It works!</strong>',
+    });
+
+    console.log('info: ', info)
+
+    res.json({
+        "message": "ok",
+        to
+    })
+
+})
+
+app.get("/check_queue", async (req, res) => {
+
+    const randomId = Math.floor(Math.random() * 10) + 1;
+    console.log(`add queue check ${randomId}`)
+
+    await check(randomId)
+
+    res.json({
+        "message": "queue ok"
+    })
 })
 
 app.listen(4300, () => {
